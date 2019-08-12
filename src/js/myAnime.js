3fc8bcac945a49202bb46e4d058d2d09305ff58b
PaulKline <trb34074@yandex.ru>
2019-08-13 03:25:14 +0500

    загрузил анимацию на css

--- src/js/myAnime.js
+++ src/js/myAnime.js
@@ -20,4 +20,31 @@ $(document).ready(function(){
       };
     };
   });
+  $(window).scroll(function () {
+    if ($(this).width() > 1200) {
+      if ($(this).scrollTop() >= 2300) {
+        $(".steps").css("animation-name", "hiddenSteps"),
+        $(".steps").css("animation-duration", "5s")
+        $(".steps").css("animation-fill-mode:", "forwards")
+      }; 
+    };
+  });
+  $(window).scroll(function () {
+    if ($(this).width() > 1200) {
+      if ($(this).scrollTop() >= 3200) {
+        $(".helpful").css("animation-name", "helpfullBack"),
+        $(".helpful").css("animation-duration", "3s")
+        $(".helpful").css("animation-fill-mode:", "forwards")
+      };
+    };
+  });
+  $(window).scroll(function () {
+    if ($(this).width() > 1200) {
+      if ($(this).scrollTop() >= 500) {
+        $(".portfolio__title").css("animation-name", "titleRight"),
+        $(".portfolio__title").css("animation-duration", "3s")
+        $(".portfolio__title").css("animation-fill-mode:", "forwards")
+      };
+    };
+  });
 });
